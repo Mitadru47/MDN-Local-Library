1c391c1c5979780913ecc02576b0ba9c0457f77c
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-02-03 19:34:42 +0530

    Authorlist & Genrelist Page Implementation

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
@@ -33,6 +34,16 @@ AuthorSchema.virtual("url").get(function (){
 
     // We don't use an arrow function here as we'll need the this object
     return `/catalog/author/${this._id}`;
+});
+
+// Virtual for author's date_of_birth field
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : "";
+});
+
+// Virtual for author's date_of_death field
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : "";
 });
 
 // Export model
